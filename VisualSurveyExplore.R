9d102f5019b207330696204ab4922ac74fa49023
annikaputt <annika@instream.net>
2016-01-07 14:44:55 -0800

    Updated master

--- VisualSurveyExplore.R
+++ VisualSurveyExplore.R
@@ -9,6 +9,15 @@ library(lubridate)
 # source the import file to obtain "fishid" data frane
 source("VisualSurveyImport.r")
 master <- walks
+
+# Create axis labels
+axisdates <- format(seq(as.POSIXlt("2010-01-01"), length=12, by="1 month"),"%b-%d")
+axisdatesdays <- as.POSIXlt(seq(as.POSIXlt("2010-01-01"), length=12, by="1 month"))$yday
+
+# Create a date range for plotting and subsetting
+kodates <- c(212,275)
+rbdates <- c(120,180)
+
 ##########
 ### For 2014 I only had counts for one section. In other years I had above and below.
 # plot the number of fish seen during the stream walk by location for each species in Carpenter Res
@@ -17,78 +26,96 @@ master <- walks
 kosubset <- subset(walks, stream %in% c("truax creek","girl creek","macdonald creek", "sucker creek", "marshall creek"))
 # Only plot 2014 for now
 kosubset$year <- format(kosubset$date, "%Y")
-kosubset <- subset(kosubset,year=="2014")
-kosubset2 <- subset(kosubset,date > "2014-07-01")
-ko  <- ggplot(data=kosubset2, aes(x=date, y=kocount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
+kosubset$daynumber <- as.POSIXlt(kosubset$date)$yday
+kosubset2014 <- subset(kosubset,year=="2014")
+kosubset2014 <- subset(kosubset2014,daynumber>kodates[1] & daynumber<kodates[2])
+kosubset2015 <- subset(kosubset,year=="2015")
+kosubset2015 <- subset(kosubset2015,daynumber>kodates[1] & daynumber<kodates[2])
+ko2014  <- ggplot(data=kosubset2014, aes(x=date, y=kocount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
+ko2015  <- ggplot(data=kosubset2015, aes(x=date, y=kocount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
 
-# Create a base plot version of the kokanee plot
+
+# Create a base plot version of the kokanee plot for both years
 windows()
 
-Months        <- as.Date(as.vector(as.character(seq.Date(
-  as.Date(strptime(140701,"%y%m%d")),
-  as.Date(strptime(141030,"%y%m%d")),
-  by="week"))),format="%Y-%m-%d")
-Months        <- Months[seq(1,length(Months),by=1)]
-labels <- format(Months, "%d-%b-%y")
-
-kosubset2 <- subset(kosubset,date > "2014-07-01" & date < "2014-09-30")
-
-plot(subset(kosubset2,stream=="truax creek")$date,subset(kosubset2,stream=="truax creek")$kocount.above,xlab="",ylab="Kokanee Count",
-     las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,120))
-  points(subset(kosubset2,stream=="girl creek")$date,subset(kosubset2,stream=="girl creek")$kocount.above,xlab="",ylab="",
+plot(subset(kosubset2014,stream=="truax creek")$daynumber,subset(kosubset2014,stream=="truax creek")$kocount,xlab="",ylab="Kokanee Count",
+     las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,120),xlim=kodates)
+  points(subset(kosubset2014,stream=="girl creek")$daynumber,subset(kosubset2014,stream=="girl creek")$kocount,xlab="",ylab="",
         las=1,xaxt="n",pch=19,type="b",col="red")
-  points(subset(kosubset2,stream=="macdonald creek")$date,subset(kosubset2,stream=="macdonald creek")$kocount.above,xlab="",ylab="",
+  points(subset(kosubset2014,stream=="macdonald creek")$daynumber,subset(kosubset2014,stream=="macdonald creek")$kocount,xlab="",ylab="",
         las=1,xaxt="n",pch=19,type="b",col="darkgrey")
-  points(subset(kosubset2,stream=="sucker creek")$date,subset(kosubset2,stream=="sucker creek")$kocount.above,xlab="",ylab="",
+  points(subset(kosubset2014,stream=="sucker creek")$daynumber,subset(kosubset2014,stream=="sucker creek")$kocount,xlab="",ylab="",
         las=1,xaxt="n",pch=19,type="b",col="blue")
-  points(subset(kosubset2,stream=="marshall creek")$date,subset(kosubset2,stream=="marshall creek")$kocount.above,xlab="",ylab="",
+  points(subset(kosubset2014,stream=="marshall creek")$daynumber,subset(kosubset2014,stream=="marshall creek")$kocount,xlab="",ylab="",
         las=1,xaxt="n",pch=19,type="b",col="purple")
-  axis(side=1,at=Months,labels=labels,cex.axis=0.8)
-  legend(max(Months)-48,120,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Sucker Creek","Truax Creek"), pch=c(19,19,19,19,19),bty="n",
+  axis(side=1,at=axisdatesdays,labels=axisdates,cex.axis=0.8)
+  legend(kodates[1],120,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Sucker Creek","Truax Creek"), pch=c(19,19,19,19,19),bty="n",
          col=c("red","darkgrey","purple","blue","black"))
+
+windows()
+
+plot(subset(kosubset2015,stream=="truax creek")$daynumber,subset(kosubset2015,stream=="truax creek")$kocount,xlab="",ylab="Kokanee Count",
+     las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,120),xlim=kodates)
+points(subset(kosubset2015,stream=="girl creek")$daynumber,subset(kosubset2015,stream=="girl creek")$kocount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="red")
+points(subset(kosubset2015,stream=="macdonald creek")$daynumber,subset(kosubset2015,stream=="macdonald creek")$kocount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="darkgrey")
+points(subset(kosubset2015,stream=="sucker creek")$daynumber,subset(kosubset2015,stream=="sucker creek")$kocount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="blue")
+points(subset(kosubset2015,stream=="marshall creek")$daynumber,subset(kosubset2015,stream=="marshall creek")$kocount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="purple")
+axis(side=1,at=axisdatesdays,labels=axisdates,cex.axis=0.8)
+legend(kodates[1],120,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Sucker Creek","Truax Creek"), pch=c(19,19,19,19,19),bty="n",
+       col=c("red","darkgrey","purple","blue","black"))
+
 
 ##########
 # Do a base plot for rb
 
-# Only plot 2014 for now
-rbsubset <- subset(walks,date > "2014-03-01" & date < "2014-08-19")
+# Remove some of the streams with counts of zero to make the plot a bit cleaner
+rbsubset <- subset(walks, stream %in% c("truax creek","girl creek","macdonald creek", "marshall creek"))
 rbsubset$year <- format(rbsubset$date, "%Y")
-rbsubset <- subset(rbsubset, stream %in% c("truax creek","girl creek","macdonald creek", "marshall creek"))
-rbsubset <- rbsubset[order(rbsubset$date, - as.numeric(rbsubset$date)), ] 
+rbsubset$daynumber <- as.POSIXlt(rbsubset$date)$yday
+rbsubset2014 <- subset(rbsubset,year=="2014")
+rbsubset2014 <- subset(rbsubset2014,daynumber>rbdates[1] & daynumber<rbdates[2])
+rbsubset2015 <- subset(rbsubset,year=="2015")
+rbsubset2015 <- subset(rbsubset2015,daynumber>rbdates[1] & daynumber<rbdates[2])
+rb2014  <- ggplot(data=rbsubset2014, aes(x=date, y=rbcount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
+rb2015  <- ggplot(data=rbsubset2015, aes(x=date, y=rbcount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
 
-rb  <- ggplot(data=rbsubset, aes(x=date, y=rbcount.above,  group=stream, colour=stream)) + geom_line() + geom_point()
 
+# Create a base plot version of the rainbow plot for both years
+windows()
+
+plot(subset(rbsubset2014,stream=="truax creek")$daynumber,subset(rbsubset2014,stream=="truax creek")$rbcount,xlab="",ylab="Rainbow Count",
+     las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,13),xlim=rbdates)
+points(subset(rbsubset2014,stream=="girl creek")$daynumber,subset(rbsubset2014,stream=="girl creek")$rbcount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="red")
+points(subset(rbsubset2014,stream=="macdonald creek")$daynumber,subset(rbsubset2014,stream=="macdonald creek")$rbcount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="darkgrey")
+points(subset(rbsubset2014,stream=="marshall creek")$daynumber,subset(rbsubset2014,stream=="marshall creek")$rbcount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="purple")
+axis(side=1,at=axisdatesdays,labels=axisdates,cex.axis=0.8)
+legend(rbdates[1],13,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Truax Creek"), pch=c(19,19,19,19,19),bty="n",
+       col=c("red","darkgrey","purple","black"))
 
 windows()
 
-MonthsRB    <- as.Date(as.vector(as.character(seq.Date(
-  as.Date(strptime(140301,"%y%m%d")),
-  as.Date(strptime(140820,"%y%m%d")),
-  by="week"))),format="%Y-%m-%d")
-MonthsRB    <- MonthsRB[seq(1,length(MonthsRB),by=1)]
-labelsRB <- format(MonthsRB, "%d-%b-%y")
-
-
-
-plot(subset(rbsubset,stream=="marshall creek")$date,subset(rbsubset,stream=="marshall creek")$rbcount.above,xlab="",ylab="Rainbow Count",
-     las=1,xaxt="n",pch=19,type="b",col="purple",ylim=c(0,12))
-points(subset(rbsubset,stream=="girl creek")$date,subset(rbsubset,stream=="girl creek")$rbcount.above,xlab="",ylab="",
+# This plot sucks becuase there is a bunch of overlap as it is mostly zero
+plot(subset(rbsubset2015,stream=="truax creek")$daynumber,subset(rbsubset2015,stream=="truax creek")$rbcount,xlab="",ylab="Rainbow Count",
+     las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,13),xlim=rbdates)
+points(subset(rbsubset2015,stream=="girl creek")$daynumber,subset(rbsubset2015,stream=="girl creek")$rbcount,xlab="",ylab="",
        las=1,xaxt="n",pch=19,type="b",col="red")
-points(subset(rbsubset,stream=="macdonald creek")$date,subset(rbsubset,stream=="macdonald creek")$rbcount.above,xlab="",ylab="",
+points(subset(rbsubset2015,stream=="macdonald creek")$daynumber,subset(rbsubset2015,stream=="macdonald creek")$rbcount,xlab="",ylab="",
        las=1,xaxt="n",pch=19,type="b",col="darkgrey")
-points(subset(rbsubset,stream=="truax creek")$date,subset(rbsubset,stream=="truax creek")$rbcount.above,xlab="",ylab="",
-       las=1,xaxt="n",pch=19,type="b",col="black")
-axis(side=1,at=MonthsRB,labels=labelsRB,cex.axis=0.8)
-legend(max(MonthsRB)-75,12,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Truax Creek"), pch=c(19,19,19,19),bty="n",
+points(subset(rbsubset2015,stream=="marshall creek")$daynumber,subset(rbsubset2015,stream=="marshall creek")$rbcount,xlab="",ylab="",
+       las=1,xaxt="n",pch=19,type="b",col="purple")
+axis(side=1,at=axisdatesdays,labels=axisdates,cex.axis=0.8)
+legend(rbdates[1],13,legend=c("Girl Creek","Macdonald Creek","Marshall Creek","Truax Creek"), pch=c(19,19,19,19,19),bty="n",
        col=c("red","darkgrey","purple","black"))
-
 #####################
 # Plot the sum of all streams for ko and rainbow by date
 #####################
-
-# Create axis labels
-axisdates <- format(seq(as.POSIXlt("2010-01-01"), length=12, by="1 month"),"%b-%d")
-axisdatesdays <- as.POSIXlt(seq(as.POSIXlt("2010-01-01"), length=12, by="1 month"))$yday
                        
 # Add day numbers to the file so that plotting can be done for multiple years
 datesum$daynumber <- as.POSIXlt(datesum$date)$yday
@@ -96,18 +123,18 @@ datesum$year <- format(datesum$date,"%Y")
 datesum2014 <- subset(datesum,year=="2014")
 datesum2015 <- subset(datesum,year=="2015")
 
-kodates <- c(212,275)
-rbdates <- c(120,180)
 ko2014 <- subset(datesum2014,daynumber>kodates[1] & daynumber<kodates[2])
 ko2015 <- subset(datesum2015,daynumber>kodates[1] & daynumber<kodates[2])
 rb2014 <- subset(datesum2014,daynumber>rbdates[1] & daynumber<rbdates[2])
 rb2015 <- subset(datesum2015,daynumber>rbdates[1] & daynumber<rbdates[2])
 
+windows()
 plot(ko2014$daynumber,ko2014$kosums,ylab="Count in all Streams",xlab="",las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,300),xlim=kodates,lwd=2)
 points(ko2015$daynumber,ko2015$kosums,ylab="",xlab="",las=1,xaxt="n",pch=19,type="b",lty=2,col="black",lwd=2)
 axis(1,at=axisdatesdays,labels=axisdates)
 legend(kodates[1],300,lty=c(1,2),lwd=c(2,2),legend=c("2014","2015"),bty="n")
 
+windows()
 plot(rb2014$daynumber,rb2014$rbsums,ylab="Count in all Streams",xlab="",las=1,xaxt="n",pch=19,type="b",col="black",ylim=c(0,16),xlim=rbdates,lwd=2)
 points(rb2015$daynumber,rb2015$rbsums,ylab="",xlab="",las=1,xaxt="n",pch=19,type="b",lty=2,col="black",lwd=2)
 axis(1,at=axisdatesdays,labels=axisdates)
